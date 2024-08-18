6b4d14091e1c59a4d88dcf5201450be664987714
Abdullah Yilmaz <48772653+abdullahyilmaz0@users.noreply.github.com>
2024-08-18 23:34:39 +0200

    Add files via upload

---
+++ banka.py
@@ -0,0 +1,62 @@
+import os
+import random
+import csv
+
+hesaplar={ }
+def olustur():
+    global hesaplar
+    ad =input("isminizi giriniz...:")
+    soyad=input("soyadinizi giriniz...")
+    bakiye=int(input("bakiye girisi yapiniz...:"))
+    hn=f"{ad[0].upper()}{soyad[0].upper()}{random.randint(100000,999999)}"
+    hesaplar[hn]={"ad": ad, "soyad": soyad, "bakiye": bakiye}
+
+    
+    with open('data.csv', mode='a', newline='', encoding='utf-8') as file:
+        writer = csv.writer(file)
+        writer.writerow(["Hesap Numarası", "Ad", "Soyad", "Bakiye"])
+    
+        for hesap_no, bilgiler in hesaplar.items():
+            writer.writerow([hesap_no, bilgiler["ad"], bilgiler["soyad"], bilgiler["bakiye"]])
+
+    print("Hesap basariyla kaydedildi.")
+    
+    os.system('cls')
+def görüntüle():
+    print()
+def bakiye_görüntüle():
+    print()
+def para_yatir():
+    print()
+def para_cek():
+    print()
+def transfer():
+    print()
+def exit():
+    print()
+os.system('cls')
+print("""
+1- Hesap Bilgileri Görüntüle
+2- Bakiye Görüntüle
+3- Para Yatirma
+4- Para Cekme
+5- Para Transferi
+6- Hesap Olusturma
+7- Cikis
+ """)
+sec=input("LÜtfen Islem Seciniz...:")
+if sec == "1":
+    görüntüle()
+elif sec=="2":
+    bakiye_görüntüle()
+elif sec=="3":
+    para_yatir()
+elif sec=="4":
+    para_cek()
+elif sec=="5":
+    transfer()
+elif sec=="6":
+    olustur()
+elif sec=="7":
+    exit()
+    
